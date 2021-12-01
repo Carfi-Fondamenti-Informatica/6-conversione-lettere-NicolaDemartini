9a369957e45d91a66236b35b19019e6ee23c0a30
NicolaDemartini <91873902+NicolaDemartini@users.noreply.github.com>
2021-12-01 20:59:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-int main(){
-    char a=a;
-    cin >> a;
-    if(converti_carattere(a) == true){
-        cout << a << endl;
-    }else{
-        cout << "errore" << endl;
-    }
-    return 0;
+int main() {
+    char c;
+    cin>>c;
+    if (Verifica(c) == 1){
+        cout << char (Conversione(c)) << endl;
+      
+    } else {
+          cout << "errore" << endl;
+      }
+  return 0;
 }
