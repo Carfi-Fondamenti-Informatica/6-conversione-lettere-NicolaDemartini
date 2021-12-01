f359a0bb044fa81d2ec6a570f4099efa74d35d31
NicolaDemartini <91873902+NicolaDemartini@users.noreply.github.com>
2021-12-01 20:32:59 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+#include "lib.h"
+bool converti_carattere (char &a){
+    bool w = true;
+    if((a >= 65 && a <= 90)or(a >= 97 && a <= 122)){
+        w = true;
+        if(a >= 65 && a <= 90){
+            a=a+32;
+            return a;
+        }else{
+            a=a-32;
+            return a;
+        }
+    }else{
+        w=false;
+    }
+    return w;
+}
